65a42f75d3e99a108325d4bc4f5204a263aa550c
ByoungYoul Song <parasby@gmail.com>
2021-12-02 17:39:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# ROS-WS-GATEWAY Example(ros-ws-gateway with Cloisim)
+
+## BUILD
+```
+docker-compose build
+```
+## RUN
+```
+xhost +
+docker-compose up
+```
+
+## TEST
+Open "http://localhost:9001/docs" with Web Browser
---
+++ run.sh
@@ -0,0 +1,18 @@
+# support NVIDIA 
+#!/bin/bash
+xhost +
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    sudo chmod a+r $XAUTH
+fi
+docker-compose build
+docker-compose up
+xhost -
